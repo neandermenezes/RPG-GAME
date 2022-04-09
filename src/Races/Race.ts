5ad928c30ccf934903d22a30a481887d50456d82
neandermenezes <neander2801@gmail.com>
2022-04-09 15:25:12 -0300

    req 4

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,11 +1,8 @@
 abstract class Race {
-  private _name: string;
-  private _dexterity: number;
-
-  constructor(name: string, dexterity: number) { 
-    this._name = name;
-    this._dexterity = dexterity;
-  }
+  constructor(
+    private _name: string, 
+    private _dexterity: number,
+  ) {}
 
   public get name(): string {
     return this._name;
