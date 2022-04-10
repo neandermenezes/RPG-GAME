6fb160f5e9813e97b675591d87c275de51158e45
neandermenezes <neander2801@gmail.com>
2022-04-10 12:48:15 -0300

    req 9

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -45,7 +45,7 @@ class Character implements Fighter {
     if (this._lifePoints < 0) this._lifePoints = -1;
   }
 
-  public attack(enemy: Fighter): void {
+  public attack(enemy: Fighter | SimpleFighter): void {
     const e = enemy;
     e.lifePoints -= this._strength;
   }
---
+++ src/Monster.ts
@@ -0,0 +1,34 @@
+import { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  constructor(
+    private _lifePoints: number = 85,
+    private _strength: number = 63,
+  ) {}
+
+  get lifePoints(): number { return this._lifePoints; }
+  get strength(): number { return this._strength; }
+
+  attack(enemy: SimpleFighter): void {
+    const e = enemy;
+    e.lifePoints -= this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints;
+ 
+    let updatedLife = 0;
+
+    if (damage > 0) {
+      updatedLife = this._lifePoints - damage;
+    }
+
+    if (updatedLife <= 0) updatedLife = -1;
+
+    this._lifePoints = updatedLife;
+
+    return this._lifePoints;
+  }
+}
+
+export default Monster;
